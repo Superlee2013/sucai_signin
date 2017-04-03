f4fdbbb9997a0d23e4ec6908f6713ed1ce510b24
Superlee <superlee4life@foxmail.com>
2017-04-03 15:42:55 +0800

    update time

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const SignIn = require("./SignIn");
 let signIn = new SignIn(data.userInfo.email, data.userInfo.password, "", data.host, data.loginPath, data.signInPath);
 
 
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
 signIn.createTokenPromise()
     .then(function(data) {
         // 设置获得的token值
@@ -16,6 +32,7 @@ signIn.createTokenPromise()
         // 开始登录
         signIn.createLoginPromise()
             .then(function(data) {
+                console.log(new Date().Format("yyyy-MM-dd hh:mm:ss"));
                 console.log("用户 " + signIn.getUserEmail() + " 登录成功，签到中...");
                 signIn.getMark(data);
             }, function() {
